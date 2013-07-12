99510bbb03cbb964d96f438672afa6cc30c0b0f4
Daniel Manning <daniel.manning@demandmedia.com>
2013-07-12 17:28:14 -0500

    wtf this isnt working

--- Source/Game1.cs
+++ Source/Game1.cs
@@ -26,9 +26,9 @@ namespace ResolutionBuddyExample
 
 			// Change Virtual Resolution 
 
-			Resolution.SetDesiredResolution(320, 240);
+			//Resolution.SetDesiredResolution(320, 240);
 			//Resolution.SetDesiredResolution(640, 480);
-			//Resolution.SetDesiredResolution(1024, 768);
+			Resolution.SetDesiredResolution(1024, 768);
 
 			//Resolution.SetScreenResolution(320, 240, false);
 			Resolution.SetScreenResolution(1280, 720, false);
@@ -53,9 +53,9 @@ namespace ResolutionBuddyExample
 		{
 			// Create a new SpriteBatch, which can be used to draw textures.
 			spriteBatch = new SpriteBatch(GraphicsDevice);
-			_texture = Content.Load<Texture2D>("alley_320x240");
+			//_texture = Content.Load<Texture2D>("alley_320x240");
 			//_texture = Content.Load<Texture2D>("alley_640x480");
-			//_texture = Content.Load<Texture2D>("alley_1024x768");
+			_texture = Content.Load<Texture2D>("alley_1024x768");
 			//_texture = Content.Load<Texture2D>("Braid_screenshot8");
 		}
 
