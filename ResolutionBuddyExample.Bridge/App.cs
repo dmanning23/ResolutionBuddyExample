2ee41bf29b8ea16188d588bdb9b5d137d6901e70
Dan Manning <dmanning@oceaneering.com>
2021-11-06 00:28:52 -0500

    added bridge example

---
+++ ResolutionBuddyExample.Bridge/App.cs
@@ -0,0 +1,34 @@
+using Bridge;
+using System;
+using System.IO;
+using static Retyped.dom;
+using Console = System.Console;
+using Microsoft.Xna.Framework;
+using ResolutionBuddyExample;
+
+namespace ResolutionBuddyExample.Bridge
+{
+    public class App
+    {
+		private static Game game;
+		
+        public static void Main()
+		{
+			var canvas = new HTMLCanvasElement();
+			canvas.width = 800;
+			canvas.height = 480;
+			canvas.id = "monogamecanvas";
+			document.body.appendChild(canvas);
+
+			//document.body.appendChild(new HTMLBRElement());
+
+			//var can = new HTMLCanvasElement();
+			//can.width = 800;
+			//can.height = 1024;
+			//document.body.appendChild(can);
+
+			game = new Game1();
+			game.Run();
+        }
+    }
+}
